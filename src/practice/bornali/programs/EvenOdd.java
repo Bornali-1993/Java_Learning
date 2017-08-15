d78f20676ecf6879a9e336251e5b98dd5b0cf04f
samarth <samarth@192.168.1.3>
2017-08-15 14:02:38 +0530

    JIRA-123 : pract...EvenOdd

--- src/practice/bornali/programs/EvenOdd.java
+++ src/practice/bornali/programs/EvenOdd.java
@@ -6,7 +6,7 @@ public class EvenOdd {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter any number: ");
+		System.out.print("Please..... Enter any number: ");
 		int n = in.nextInt();
 		if (n % 2 == 0)
 			System.out.println("Entered Number is an Even Number");
