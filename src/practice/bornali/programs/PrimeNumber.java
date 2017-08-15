3b9a428f466b920a01d42daa1aed5c053b6dee7a
samarth <samarth@192.168.1.3>
2017-08-15 14:08:00 +0530

    Practice: Prime Number

--- src/practice/bornali/programs/PrimeNumber.java
+++ src/practice/bornali/programs/PrimeNumber.java
@@ -23,7 +23,7 @@ public class PrimeNumber {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.print("Please enter a number: ");
+		System.out.print("Enter a number: ");
 		int n = in.nextInt();
 		PrimeNumber obj = new PrimeNumber();
 		boolean prime = obj.isPrime(n);
